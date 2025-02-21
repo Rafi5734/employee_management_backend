dc6e443f4e600961490e68d161b54da181a5a0dc
Rafi5734 <kamrulhasun5734@gmail.com>
2025-02-21 12:22:34 +0600

    employee model properties value changed

--- models/EmployeeModel.js
+++ models/EmployeeModel.js
@@ -6,11 +6,16 @@ const EmployeeSchema = new mongoose.Schema(
     employeeName: { type: String, required: true },
     employeeEmail: { type: String, required: true, unique: true },
     employeePhoneNumber: { type: String, required: true },
-    employeeStatus: { type: String, enum: ["Active", "Inactive", "Vacation"], default: "Active" },
+    employeeStatus: {
+      type: String,
+      enum: ["Active", "Inactive", "Vacation"],
+      default: "Inactive",
+      required: true,
+    },
     employeeDepartment: { type: String, required: true },
     employeeAddress: { type: String, required: true },
     employeeProfilePicture: { type: String, required: true }, // URL link
-    employeeCoverImage: { type: String, required: true } // URL link
+    employeeCoverImage: { type: String, required: true }, // URL link
   },
   { timestamps: true }
 );
